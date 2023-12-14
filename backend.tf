124a90a391359ed4ed3192706348bea4c9a7b447
juanfiki <juanfiquitiva@hotmail.com>
2023-12-13 20:18:31 -0500

    add code

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
-  backend "s3"{
-    bucket                 = "mundojuan960105"
-    region                 = "us-east-2"
-    key                    = "backend.tfstate"
-    dynamodb_table         = "terraformstatelock"
+  backend "s3" {
+    bucket         = "mundojuan960105"
+    region         = "us-east-2"
+    key            = "backend.tfstate"
+    dynamodb_table = "terraformstatelock"
   }
 }
 
--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 ##Create and bootstrap webserver #lanzar
 resource "aws_instance" "webserver" {
-  ami                         = "ami-06d4b7182ac3480fa"
-  instance_type               = "t2.micro"
+  ami           = "ami-06d4b7182ac3480fa"
+  instance_type = "t2.micro"
   #key_name                    = app-ssh-key 
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.sg.id]
   subnet_id                   = aws_subnet.subnet.id
-  user_data                   = "${file("create_apache.sh")}"
+  user_data                   = file("create_apache.sh")
 
   tags = {
     Name = "webserver"
